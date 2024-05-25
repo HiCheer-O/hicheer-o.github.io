e382118534bba8eda41874d39b40be03d28f124d
HiCheer-O <chendangdang1998@gmail.com>
2024-05-25 23:52:19 +0800

    banner update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,7 +22,7 @@ export default defineUserConfig({
 		// 作者信息
 		avatar: {
 			// 头像
-			url: 'https://coupon.heke100.com/hk-data/2024/05/25/c413911d6b8d4689a09edd3b7cd5626e.png',
+			url: 'https://coupon.heke100.com/hk-data/2024/05/25/3c3d44c8a08343fda93204a1d121b852.png',
 			// 昵称
 			name: 'HiCheer',
 			// 描述
